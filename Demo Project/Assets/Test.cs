023f50363ff2fa1afa78befe4409af9ed0fda4fe
Krishna Kumar <86921469+Krsna-Kumar@users.noreply.github.com>
2023-09-09 16:23:05 +0530

    Press space to jump added

--- Demo Project/Assets/Test.cs
+++ Demo Project/Assets/Test.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    [SerializeField] private float upSpeed;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up *  upSpeed, ForceMode2D.Impulse);
+        }
     }
 }
